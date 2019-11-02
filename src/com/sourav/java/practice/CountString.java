4a6b9c80c2138ed72171e9a6419c6ed88317ce05
srvkm699 <srvkmr699>
2019-11-02 22:13:59 +0530

    second commit

--- src/com/sourav/java/practice/CountString.java
+++ src/com/sourav/java/practice/CountString.java
@@ -3,7 +3,7 @@ package com.sourav.java.practice;
 import java.util.Scanner;
 
 public class CountString {
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter string value");
@@ -13,26 +13,25 @@ public class CountString {
 		System.out.println(countString(inputString));
 		scanner.close();
 	}
+
 	public static String countString(String inputString) {
 		String temproraryString = "";
 		String resultantString = "";
 		int counter = 1;
 		int incrementor;
-		for(incrementor = 0;incrementor<inputString.length();incrementor++) {
-			if(incrementor >= inputString.length()-1) {
-				temproraryString = inputString.charAt(incrementor)+" "+counter;
+		for (incrementor = 0; incrementor < inputString.length(); incrementor++) {
+			if (incrementor >= inputString.length() - 1) {
+				temproraryString = inputString.charAt(incrementor) + "" + counter;
 				resultantString = resultantString.concat(temproraryString);
 				break;
-			}
-			else if(inputString.charAt(incrementor) == inputString.charAt(incrementor+1)) {
+			} else if (inputString.charAt(incrementor) == inputString.charAt(incrementor + 1)) {
 				counter++;
 				continue;
 			}
-			temproraryString = inputString.charAt(incrementor)+""+counter;
+			temproraryString = inputString.charAt(incrementor) + "" + counter;
 			resultantString = resultantString.concat(temproraryString);
 			counter = 1;
 		}
-		
 		return resultantString.toString();
 	}
 }
